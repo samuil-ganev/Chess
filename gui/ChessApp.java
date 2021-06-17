37f16d0cc9e098ba40e0121656127ad67c8fdfa4
Samuil Ganev <57064926+samuil-ganev@users.noreply.github.com>
2021-06-17 23:10:31 +0300

    Update ChessApp.java

--- gui/ChessApp.java
+++ gui/ChessApp.java
@@ -1,18 +1,23 @@
 package gui;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class ChessApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame();
-		 frame.setSize(400, 400);
-		 frame.add(new BoardControl());
-		 frame.setTitle("Chess");
-		 frame.setLocationRelativeTo(null);
-		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 frame.setVisible(true);
+		frame.setSize(670, 700);
+		frame.add(new BoardControl());
+		frame.setTitle("Chess");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		 
+		Image icon = Toolkit.getDefaultToolkit().getImage("resources/icon.png");  
+		frame.setIconImage(icon);  
+		 
 	}
 
 }
